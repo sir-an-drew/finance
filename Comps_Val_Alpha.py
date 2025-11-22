a7fed9f3412bc69c7ccc964eb46e8bf2c71a1e08
sir-an-drew <140798188+sir-an-drew@users.noreply.github.com>
2025-11-22 14:46:47 +0000

    Update Comps_Val_Alpha.py

--- Comps_Val_Alpha.py
+++ Comps_Val_Alpha.py
@@ -14,7 +14,7 @@ if not api_key:
 # --- App ---
 st.set_page_config(page_title="Comps Valuation - Alpha Vantage", layout="wide")
 st.title("Comparable Company Valuation")
-st.caption("Using Alpha Vantage • Andrew Syropiatov - syropiatov@bluestoneib.com")
+st.caption("Using Alpha Vantage • By sir.andrew @ me.com")
 
 # Target Company Input
 st.subheader("Target Company (Manual Input)")
