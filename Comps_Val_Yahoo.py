f3499f32195163ab6c0b41a8b2eb713fa4cf9c79
sir-an-drew <140798188+sir-an-drew@users.noreply.github.com>
2025-11-22 11:13:02 +0000

    Update Comps_Val_Yahoo.py

--- Comps_Val_Yahoo.py
+++ Comps_Val_Yahoo.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.set_page_config(page_title="Comps Valuation", layout="wide")
 st.title("Comparable Company Valuation")
-st.caption("Using Yahoo Finance API yfinance | Andrew Syropiatov - syropiatov@bluestone.ib")
+st.caption("Using Yahoo Finance API yfinance | Andrew Syropiatov - syropiatov@bluestoneib.com")
 
 # Input fields
 st.subheader("Target Company (Manual Input)")
